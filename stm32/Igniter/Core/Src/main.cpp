f9c4b6dc280b492b99f588116929510a1435fd2f
Yuuki Fujihara <mail@fujiy.dev>
2022-09-16 10:09:39 +0900

    Add igniter module

---
+++ stm32/Igniter/Core/Src/main.cpp
@@ -0,0 +1,747 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+#include <stdlib.h>
+#include <cstdio>
+#include "command.hpp"
+#include "diagnostics.hpp"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+#define SAMPLE_FREQ 100
+#define TLM_FREQ 1
+
+//#define LAUNCH_CONFIRM_TIMEOUT_TICK 2000
+#define LAUNCH_CONFIRM_KEEP_TICK 5000
+
+#define BATTERY_V_MIN 5.0
+
+#define CAN_SEND_TIMEOUT_TICK 50
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ ADC_HandleTypeDef hadc;
+DMA_HandleTypeDef hdma_adc;
+
+CAN_HandleTypeDef hcan;
+
+IWDG_HandleTypeDef hiwdg;
+
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim14;
+
+/* USER CODE BEGIN PV */
+
+Diagnostics diag(MODULE_IGNITER);
+
+CANChannel can;
+
+CAN_TxHeaderTypeDef   CanTxHeader;
+CAN_RxHeaderTypeDef   CanRxHeader;
+uint8_t               CanTxData[8];
+uint8_t               CanRxData[8];
+uint32_t              CanTxMailbox;
+
+
+uint16_t adc_values[12];
+
+float battery_voltage;
+float igniter_current;
+float igniter_voltage;
+
+bool launch_ok = false;
+bool launch_confirm_tick;
+
+uint32_t count = 0;
+bool ignite = false;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_CAN_Init(void);
+static void MX_DMA_Init(void);
+static void MX_IWDG_Init(void);
+static void MX_ADC_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_TIM14_Init(void);
+/* USER CODE BEGIN PFP */
+
+extern "C" void initialise_monitor_handles(void);
+void CAN_Send();
+void CAN_Received();
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if (htim == &htim2) {
+    	volatile float voltages[10];
+    	for (int i = 0; i < 10; i++) {
+    		voltages[i] = (float)adc_values[i] * 3.3 / 4095;
+
+//    		printf("%d %d\n", i, (int)(voltages[i] * 1000));
+    	}
+
+    	battery_voltage = voltages[0] * 11.0;
+    	igniter_current = voltages[1];
+    	igniter_voltage = voltages[2];
+
+    	count++;
+
+
+#ifndef DEBUG
+	  HAL_IWDG_Refresh(&hiwdg);
+#endif
+    }
+    else if (htim == &htim14) {
+
+    }
+}
+
+void CAN_Received() {
+	uint32_t raw;
+	int32_t mid = can.rx.getDiag(raw);
+	if (mid >= 0) diag.update(mid, raw, HAL_GetTick());
+
+	if (can.rx.id == 'm') {
+		if (can.rx.get('N')) launch_ok = false;
+		else if (can.rx.get('A')) {
+			if (!launch_ok) launch_confirm_tick = HAL_GetTick();
+			launch_ok = true;
+		}
+	}
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+#ifdef DEBUG
+  initialise_monitor_handles();
+#endif
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_CAN_Init();
+  MX_DMA_Init();
+  MX_IWDG_Init();
+  MX_ADC_Init();
+  MX_TIM2_Init();
+  MX_TIM14_Init();
+  /* USER CODE BEGIN 2 */
+
+#ifdef DEBUG
+  printf("Start\n");
+#endif
+
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+
+  HAL_ADCEx_Calibration_Start(&hadc);
+  HAL_ADC_Start_DMA(&hadc, (uint32_t*)adc_values, 12);
+
+  HAL_Delay(900);
+
+  HAL_TIM_Base_Start_IT(&htim2);
+  HAL_TIM_PWM_Start(&htim14, TIM_CHANNEL_1);
+  __HAL_TIM_SET_COMPARE(&htim14, TIM_CHANNEL_1, 0);
+
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+	  // Launch
+	  if (launch_ok &&
+			  HAL_GetTick() - launch_confirm_tick > LAUNCH_CONFIRM_KEEP_TICK &&
+			  battery_voltage > BATTERY_V_MIN) {
+		  HAL_GPIO_WritePin(IGNITE_GPIO_Port, IGNITE_Pin, GPIO_PIN_SET);
+		  __HAL_TIM_SET_COMPARE(&htim14, TIM_CHANNEL_1, 1);
+	  }
+	  else {
+		  HAL_GPIO_WritePin(IGNITE_GPIO_Port, IGNITE_Pin, GPIO_PIN_RESET);
+		  __HAL_TIM_SET_COMPARE(&htim14, TIM_CHANNEL_1, 0);
+	  }
+
+	  diag.setStatus(STATUS_0, battery_voltage < BATTERY_V_MIN);
+	  diag.setStatus(STATUS_2, launch_ok);
+
+
+	  Command tlm('I', 0, 0, 3);
+
+	  tlm.entries[0].set('V', (float)battery_voltage);
+	  tlm.entries[1].set('C', (float)igniter_current);
+	  tlm.entries[2].set('R', (float)0.0);
+
+	  can.tx.push(tlm);
+
+	  CAN_Send();
+
+	  Command mode('M', 0, 0, 2);
+
+	  mode.entries[0].set(launch_ok ? 'A' : 'N');
+	  mode.entries[1].set('0' + MODULE_IGNITER_N, (uint32_t)diag.encode());
+
+	  can.tx.push(mode);
+
+	  CAN_Send();
+
+#ifdef DEBUG
+
+		printf("Vb: %d.%03d V\n", (int)battery_voltage, abs((int)(battery_voltage * 1000) % 1000));
+		printf("CI: %d.%03d V\n", (int)igniter_current, abs((int)(igniter_current * 1000) % 1000));
+		printf("VI: %d.%03d V\n", (int)igniter_voltage, abs((int)(igniter_voltage * 1000) % 1000));
+
+		diag.printSummary();
+#endif
+
+    	HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+
+  	  HAL_Delay(250);
+
+
+    /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI14|RCC_OSCILLATORTYPE_HSI48
+                              |RCC_OSCILLATORTYPE_LSI;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
+  RCC_OscInitStruct.HSI14CalibrationValue = 16;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC_Init(void)
+{
+
+  /* USER CODE BEGIN ADC_Init 0 */
+
+  /* USER CODE END ADC_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC_Init 1 */
+
+  /* USER CODE END ADC_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc.Instance = ADC1;
+  hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
+  hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc.Init.LowPowerAutoWait = DISABLE;
+  hadc.Init.LowPowerAutoPowerOff = DISABLE;
+  hadc.Init.ContinuousConvMode = ENABLE;
+  hadc.Init.DiscontinuousConvMode = DISABLE;
+  hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc.Init.DMAContinuousRequests = ENABLE;
+  hadc.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  if (HAL_ADC_Init(&hadc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel to be converted.
+  */
+  sConfig.Channel = ADC_CHANNEL_7;
+  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel to be converted.
+  */
+  sConfig.Channel = ADC_CHANNEL_8;
+  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel to be converted.
+  */
+  sConfig.Channel = ADC_CHANNEL_9;
+  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC_Init 2 */
+
+  /* USER CODE END ADC_Init 2 */
+
+}
+
+/**
+  * @brief CAN Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_CAN_Init(void)
+{
+
+  /* USER CODE BEGIN CAN_Init 0 */
+
+  /* USER CODE END CAN_Init 0 */
+
+  /* USER CODE BEGIN CAN_Init 1 */
+
+  /* USER CODE END CAN_Init 1 */
+  hcan.Instance = CAN;
+  hcan.Init.Prescaler = 24;
+  hcan.Init.Mode = CAN_MODE_NORMAL;
+  hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan.Init.TimeSeg1 = CAN_BS1_12TQ;
+  hcan.Init.TimeSeg2 = CAN_BS2_3TQ;
+  hcan.Init.TimeTriggeredMode = DISABLE;
+  hcan.Init.AutoBusOff = DISABLE;
+  hcan.Init.AutoWakeUp = DISABLE;
+  hcan.Init.AutoRetransmission = ENABLE;
+  hcan.Init.ReceiveFifoLocked = DISABLE;
+  hcan.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN CAN_Init 2 */
+
+
+  CAN_FilterTypeDef filter0;
+  uint32_t fId0   =  0x000 << 21;
+  uint32_t fMask0 = (0x100 << 21) | 0x4;
+  filter0.FilterIdHigh         = fId0 >> 16;
+  filter0.FilterIdLow          = fId0;
+  filter0.FilterMaskIdHigh     = fMask0 >> 16;
+  filter0.FilterMaskIdLow      = fMask0;
+  filter0.FilterScale          = CAN_FILTERSCALE_32BIT;
+  filter0.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+  filter0.FilterBank           = 0;
+  filter0.FilterMode           = CAN_FILTERMODE_IDMASK;
+  filter0.FilterActivation     = ENABLE;
+  if (HAL_CAN_ConfigFilter(&hcan, &filter0) != HAL_OK)
+  {
+	Error_Handler();
+  }
+
+  CAN_FilterTypeDef filter1;
+  uint32_t fId1   =  0x100 << 21;
+  uint32_t fMask1 = (0x100 << 21) | 0x4;
+  filter1.FilterIdHigh         = fId1 >> 16;
+  filter1.FilterIdLow          = fId1;
+  filter1.FilterMaskIdHigh     = fMask1 >> 16;
+  filter1.FilterMaskIdLow      = fMask1;
+  filter1.FilterScale          = CAN_FILTERSCALE_32BIT;
+  filter1.FilterFIFOAssignment = CAN_FILTER_FIFO1;
+  filter1.FilterBank           = 2;
+  filter1.FilterMode           = CAN_FILTERMODE_IDMASK;
+  filter1.FilterActivation     = ENABLE;
+  if (HAL_CAN_ConfigFilter(&hcan, &filter1) != HAL_OK)
+  {
+	Error_Handler();
+  }
+
+  if (HAL_CAN_Start(&hcan) != HAL_OK)
+  {
+    /* Start Error */
+    Error_Handler();
+  }
+
+  if (HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING | CAN_IT_RX_FIFO1_MSG_PENDING) != HAL_OK)
+  {
+    /* Notification Error */
+    Error_Handler();
+  }
+
+  CanTxHeader.RTR   = CAN_RTR_DATA;
+  CanTxHeader.IDE   = CAN_ID_STD;
+  CanTxHeader.TransmitGlobalTime = DISABLE;
+
+  /* USER CODE END CAN_Init 2 */
+
+}
+
+/**
+  * @brief IWDG Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_IWDG_Init(void)
+{
+
+  /* USER CODE BEGIN IWDG_Init 0 */
+
+#ifdef DEBUG
+  return;
+#endif
+
+  /* USER CODE END IWDG_Init 0 */
+
+  /* USER CODE BEGIN IWDG_Init 1 */
+
+  /* USER CODE END IWDG_Init 1 */
+  hiwdg.Instance = IWDG;
+  hiwdg.Init.Prescaler = IWDG_PRESCALER_16;
+  hiwdg.Init.Window = 4095;
+  hiwdg.Init.Reload = 4095;
+  if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN IWDG_Init 2 */
+
+  /* USER CODE END IWDG_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 48000 - 1;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 1000 / SAMPLE_FREQ - 1;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief TIM14 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM14_Init(void)
+{
+
+	/* USER CODE BEGIN TIM14_Init 0 */
+
+	/* USER CODE END TIM14_Init 0 */
+
+	TIM_OC_InitTypeDef sConfigOC = {0};
+
+	/* USER CODE BEGIN TIM14_Init 1 */
+
+	/* USER CODE END TIM14_Init 1 */
+	htim14.Instance = TIM14;
+	htim14.Init.Prescaler = 24000-1;
+	htim14.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim14.Init.Period = 2-1;
+	htim14.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	htim14.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+	if (HAL_TIM_Base_Init(&htim14) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	if (HAL_TIM_PWM_Init(&htim14) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	sConfigOC.OCMode = TIM_OCMODE_PWM1;
+	sConfigOC.Pulse = 0;
+	sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+	sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+	if (HAL_TIM_PWM_ConfigChannel(&htim14, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	__HAL_TIM_DISABLE_OCxPRELOAD(&htim14, TIM_CHANNEL_1);
+	/* USER CODE BEGIN TIM14_Init 2 */
+
+	/* USER CODE END TIM14_Init 2 */
+	HAL_TIM_MspPostInit(&htim14);
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOA, LED_Pin|TEST_Pin|IGNITE_Pin, GPIO_PIN_RESET);
+
+	/*Configure GPIO pins : LED_Pin TEST_Pin BUZZER_Pin IGNITE_Pin */
+	GPIO_InitStruct.Pin = LED_Pin|TEST_Pin|IGNITE_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+	/* Get RX message */
+	if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &CanRxHeader, CanRxData) != HAL_OK) {
+		/* Reception Error */
+		Error_Handler();
+	}
+
+	if (can.receive(CanRxHeader.StdId, CanRxData, CanRxHeader.DLC)) {
+		CAN_Received();
+	}
+}
+void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+	/* Get RX message */
+	if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO1, &CanRxHeader, CanRxData) != HAL_OK) {
+		/* Reception Error */
+		Error_Handler();
+	}
+
+	if (can.receive(CanRxHeader.StdId, CanRxData, CanRxHeader.DLC)) {
+		CAN_Received();
+	}
+}
+
+
+/* USER CODE BEGIN 4 */
+
+void CAN_Send() {
+	while (true) {
+		uint16_t std_id;
+		uint8_t len;
+
+		volatile uint32_t tick = HAL_GetTick();
+
+		while (can.isReceiving()) {
+			if (HAL_GetTick() - tick >= CAN_SEND_TIMEOUT_TICK) {
+				can.cancelReceiving();
+#ifdef DEBUG
+				printf("BUSY\n");
+#endif
+			}
+		}
+
+		tick = HAL_GetTick();
+
+		while (
+//				HAL_GetTick() - tick <= 10 ||
+				HAL_CAN_GetTxMailboxesFreeLevel(&hcan) != 3) {
+			if (HAL_GetTick() - tick >= CAN_SEND_TIMEOUT_TICK) {
+				HAL_CAN_AbortTxRequest(&hcan, CanTxMailbox);
+				can.cancelSending();
+#ifdef DEBUG
+				printf("TIMEOUT\n");
+#endif
+				return;
+			}
+		}
+
+		uint8_t remains = can.send(std_id, CanTxData, len);
+
+		if (len == 0) return;
+
+		CanTxHeader.StdId = std_id;
+		CanTxHeader.DLC = len;
+
+
+		if (HAL_CAN_AddTxMessage(&hcan, &CanTxHeader, CanTxData, &CanTxMailbox) != HAL_OK) {
+			Error_Handler();
+		}
+
+//	#ifdef DEBUG
+//		printf("Send %c(%x), %d: %c(%x) %x %x %x %x \n",
+//				CanTxHeader.StdId, CanTxHeader.StdId, CanTxHeader.DLC,
+//				CanTxData[0], CanTxData[0], CanTxData[1], CanTxData[2], CanTxData[3], CanTxData[4]);
+//	#endif
+
+		if (remains == 0) break;
+	}
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
